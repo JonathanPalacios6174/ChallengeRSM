d691b4ba0dabf84bc86bd9ff286d74abece35679
JonathanPalacios <Jonathan.Palacios6174@gmail.com>
2024-05-02 15:54:07 -0300

    Add project web and without problem when we try to call the API

---
+++ Web/Controllers/ReportController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: ReportController
+        public ActionResult Index()
+        {
+
+            try
+            {
+                //var handler = new HttpClientHandler()
+                //{
+                //    ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                //};
+
+                // var httpClient = new HttpClient(handler);
+                var httpClient = new HttpClient();
+                httpClient.BaseAddress = new Uri("http://localhost:44306/");
+
+                var response =  httpClient.GetAsync("api/SalesReport");
+               // response.en();
+
+                //var content =  response.Content.ReadAsStringAsync();
+                return View();
+            }
+            catch (HttpRequestException ex)
+            {
+
+                throw new Exception($"Error de solicitud HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception($"Error interno del servidor: {ex.Message}");
+            }
+
+
+            return View();
+        }
+
+        // GET: ReportController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReportController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReportController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReportController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReportController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReportController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ReportController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Web/Services/SalesReportServices.cs
@@ -0,0 +1,38 @@
+﻿namespace Web.Services
+{
+    public class SalesReportServices
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public SalesReportServices(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetSalesReport()
+        {
+            try
+            {
+                var httpClient = _httpClientFactory.CreateClient();
+                httpClient.BaseAddress = new Uri("http://localhost:44306/");
+
+                var response = await httpClient.GetAsync("api/SalesReport");
+                response.EnsureSuccessStatusCode(); // Esto lanzará una excepción si no se recibe un estado de éxito (200-299)
+
+                var content = await response.Content.ReadAsStringAsync();
+                return content; // Devuelve el contenido de la respuesta
+            }
+            catch (HttpRequestException ex)
+            {
+                // Maneja los errores de solicitud HTTP
+                throw new Exception($"Error de solicitud HTTP: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Maneja otros errores
+                throw new Exception($"Error interno del servidor: {ex.Message}");
+            }
+        }
+    }
+}
